a27b8cba6f0cdb693dff714963644ae28ea7218e
Ti Hall <55930349+Titane73@users.noreply.github.com>
2019-11-15 13:59:01 -0800

    Add files via upload

--- web110/big/contact.php
+++ web110/big/contact.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
- <head>
-     <title>Web Dev Examples and Inspirations by Thomas "Ti" Hall</title>
+
+<head>
+    <title>Web Dev Examples and Inspirations by Thomas "Ti" Hall</title>
     <meta charset="utf-8" />
     <meta name="robots" content="noindex,nofollow" />
     <meta name="viewport" content="width=device-width" />
@@ -11,51 +12,50 @@
     <script src="js/script.js"></script>
     <link rel="stylesheet" href="css/big.css" />
     <link rel="stylesheet" href="css/nav.css" />
-    
-    
- </head>
-    
- <body>
 
-   <main>
-    <header>
-        <h1><a href="index.html">Ti Hall: A Study in Web Development</a></h1>
+</head>
+
+<body>
+
+    <main>
+        <header>
+            <h1><a href="index.html">Ti Hall: A Study in Web Development</a></h1>
             <nav id="cssmenu">
-              <ul>
-                 <li><a href="../index.html"><i class="fa fa-fw fa-home"></i> WEB110</a></li>
-                 <li><a href="index.html"><i class="fa fa-fw fa-institution"></i> BIG</a></li>
-                  
-        <!-- Drop down for research pages  -->
-                  <li><a href="#"><i class="fas fa-book-open"></i> Research</a>
-                    <ul>
-                        <li><a href="accessibility.html"><i class="fas fa-universal-access"></i> Accessibility</a></li>
-                        
-                        <li><a href="forms.html"><i class="fab fa-wpforms"></i> Forms</a></li>
-                        
-                        <li><a href="smo.html"><i class="fas fa-icons"></i> SMO</a></li>
-                    </ul>
-                  </li>
-                  
-                 <li><a href="gallery.html"><i class="fas fa-images"></i> Gallery</a></li>
-                  
-        <!-- Drop down for tools pages  -->                  
-               <li><a href="#"> <i class="fas fa-toolbox"></i> Tools</a>                                
-                    <ul>
-                        <li><a href="calendar.html"><i class="far fa-calendar-alt"></i> Calendar</a></li>
-                        <li><a href="map.html"><i class="fas fa-globe-americas"></i> Map</a></li>
-                        <li><a href="seo.html"><i class="fab fa-searchengin"></i> SEO</a></li>
-                    </ul>
-                 </li>
-                 <li><a href="contact.php"><i class="fas fa-envelope-open-text"></i> Contact Me</a></li>
-              </ul>
+                <ul>
+                    <li><a href="../index.html"><i class="fa fa-fw fa-home"></i> WEB110</a></li>
+                    <li><a href="index.html"><i class="fa fa-fw fa-institution"></i> BIG</a></li>
+
+                    <!-- Drop down for research pages  -->
+                    <li><a href="#"><i class="fas fa-book-open"></i> Research</a>
+                        <ul>
+                            <li><a href="accessibility.html"><i class="fas fa-universal-access"></i> Accessibility</a></li>
+
+                            <li><a href="forms.html"><i class="fab fa-wpforms"></i> Forms</a></li>
+
+                            <li><a href="smo.html"><i class="fas fa-icons"></i> SMO</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a href="gallery.html"><i class="fas fa-images"></i> Gallery</a></li>
+
+                    <!-- Drop down for tools pages  -->
+                    <li>
+                        <a href="#"> <i class="fas fa-toolbox"></i> Tools</a>
+                        <ul>
+                            <li><a href="calendar.html"><i class="far fa-calendar-alt"></i> Calendar</a></li>
+                            <li><a href="map.html"><i class="fas fa-globe-americas"></i> Map</a></li>
+                            <li><a href="seo.html"><i class="fab fa-searchengin"></i> SEO</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="contact.php"><i class="fas fa-envelope-open-text"></i> Contact Me</a></li>
+                </ul>
             </nav>
-    </header>
-       
-       
-    <div class="contact">
-    <h1>Contact Form</h1>
-       
-	<?php
+        </header>
+
+        <div class="contact">
+            <h1>Contact Form</h1>
+
+            <?php
         /*
          * Below are 2 different forms to be re-used       
          * 
@@ -64,7 +64,7 @@
          */
 
         include 'includes/contact_include.php'; #site keys & code here
-    
+
         $toAddress = "thomas.ti.hall@gmail.com";  //place your/your client's email address here
         $toName = "Ti Hall"; //place your client's name here
         $website = "BIG Contact Form";  //place NAME of your client's website
@@ -73,13 +73,13 @@
         //echo loadContact('multiple.php');#demonstrates multiple form elements
 
 	?>
-    </div>
-     
-     <footer>
-      <p><small>&copy; 2019 by <a href="contact.php">Contact Thomas Hall </a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
-    </footer>
-  </main>
-     
+        </div>
 
- </body>
+        <footer>
+            <p><small>&copy; 2019 by <a href="contact.php">Contact Thomas Hall </a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+        </footer>
+    </main>
+
+</body>
+
 </html>
